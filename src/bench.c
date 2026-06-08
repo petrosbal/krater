ae52e0b4b9094a0a07c02ea0f494dd33153d095d
petrosbal <petrosb10@yahoo.gr>
2026-06-08 23:21:57 +0300

    clean up comments and usage message in bench.c

--- src/bench.c
+++ src/bench.c
@@ -26,7 +26,7 @@ int valid_results(double *C, int N) {
     for (int i = 0; i < N*N; i++) {
         if (fabs(C[i] - expected_value) > tolerance) { // found a mismatch
             printf("\nERROR\nc[i]: %10.9f\nexp:  %10.9f\ndiff: %10.9f\ntol:  %10.9f\n\n",
-                                C[i], expected_value, fabs(C[i] - expected_value), tolerance);
+                            C[i], expected_value, fabs(C[i] - expected_value), tolerance);
             return 0;
         }
     }
@@ -37,24 +37,25 @@ int valid_results(double *C, int N) {
 int main(int argc, char *argv[]) {
     if (argc < 4) {
         printf("\nUsage:\n%s <w|nw> <matrix_size> <duration_sec>\n\n", argv[0]);
-        printf("or, if you have the Makefile:\n");
-        printf("make run <w|nw> <matrix_size> <duration_sec>\n\n");
         return 1;
     }
 
+    // arguments parsing and validation
     int do_warmup = (argv[1][0] == 'w');
     int N = atoi(argv[2]);
     double duration = atof(argv[3]);
     if (N <= 0 || duration <= 0) return 1;
 
+    // matrix allocation and initialization
     double *A = malloc(N*N*sizeof(double));
     double *B = malloc(N*N*sizeof(double));
     double *C = malloc(N*N*sizeof(double));
     if (!A || !B || !C) {
-        fprintf(stderr, "error: malloc failed for N=%d\n", N);
+        fprintf(stderr, "error: malloc() failed for N=%d\n", N);
         return 1;
     }
 
+    // A and B initialization with fixed values
     for (int i=0;i<N*N;i++) {
         A[i] = VAL_A; B[i] = VAL_B;
     }
@@ -65,6 +66,7 @@ int main(int argc, char *argv[]) {
         timespec_get(&w_start, TIME_UTC);
         double w_elapsed = 0.0;
         printf("\rWarming up... "); fflush(stdout);
+
         while (w_elapsed < 5.0) {
             matrix_mult(A,B,C,N);
             timespec_get(&w_now, TIME_UTC);
@@ -73,19 +75,19 @@ int main(int argc, char *argv[]) {
     }
 
     // actual benchmark
-    printf("BENCH_START\n"); fflush(stdout); // DO NOT TOUCH. seb script needs it
+    printf("BENCH_START\n"); fflush(stdout); // DO NOT EDIT. python script needs it
     struct timespec t_start, t_now;
     timespec_get(&t_start, TIME_UTC);
     double elapsed = 0.0;
     int iterations = 0;
 
     while (elapsed < duration) {
-        matrix_mult(A,B,C,N); //this is where you can change the workload
+        matrix_mult(A,B,C,N);
         iterations++;
         timespec_get(&t_now, TIME_UTC);
         elapsed = (t_now.tv_sec - t_start.tv_sec) + (t_now.tv_nsec - t_start.tv_nsec)/1e9;
     }
-    printf("BENCH_END\n"); fflush(stdout); // DO NOT TOUCH as well. same reason
+    printf("BENCH_END\n"); fflush(stdout); // DO NOT EDIT as well. same reason
 
     double mflops = 2.0 * N * N * N * iterations / elapsed / 1e6; // 2*N^3 flops per matrix multiplication
 
